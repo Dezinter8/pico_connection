e8d53be502d4d9b36310ac970a8a20939b756b5a
Michał Chruślicki <michal.chruslicki01@gmail.com>
2024-06-07 16:26:21 +0200

    użycie wielowątkowości zamiast timera do monitorowania portu szeregowego.

--- pico_connection/serial_publisher.py
+++ pico_connection/serial_publisher.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSHistoryPolicy, QoSReliabilityPolicy, QoSDurabilityPolicy
 from geometry_msgs.msg import Point32
 import serial
+import threading
 import time
 
 class SerialSubscriber(Node):
@@ -15,10 +16,13 @@ class SerialSubscriber(Node):
             durability=QoSDurabilityPolicy.VOLATILE
         )
         self.publisher_ = self.create_publisher(Point32, 'imu_data', qos_profile)
-        self.serial_port = None
         self.serial_device_path = '/dev/serial/by-id/usb-Raspberry_Pi_Pico_E660D4A0A772982F-if00'
+        self.serial_port = None
         self.open_serial_port()
-        self.timer = self.create_timer(0.01, self.read_and_publish)
+
+        thread = threading.Thread(target=self.read_and_publish)
+        thread.daemon = True
+        thread.start()
 
     def open_serial_port(self):
         while self.serial_port is None:
@@ -26,11 +30,15 @@ class SerialSubscriber(Node):
                 self.serial_port = serial.Serial(self.serial_device_path, 115200, timeout=None)
                 self.get_logger().info('Serial port opened successfully.')
             except serial.SerialException as e:
-                self.get_logger().warn('Failed to open serial port: %s. Retrying...' % str(e))
+                self.get_logger().warn(f'Failed to open serial port: {str(e)}. Retrying...')
                 time.sleep(2)  # Czekaj 2 sekundy przed ponowną próbą
 
+
     def read_and_publish(self):
-        if self.serial_port is not None and self.serial_port.is_open:
+        while not self.serial_port or not self.serial_port.is_open:
+            self.open_serial_port()
+
+        while rclpy.ok():
             try:
                 if self.serial_port.in_waiting > 0:
                     line = self.serial_port.readline().decode('utf-8').strip()
@@ -41,31 +49,37 @@ class SerialSubscriber(Node):
                         point_msg.y = float(parts[1]) * -1
                         point_msg.x = float(parts[2])
                         self.publisher_.publish(point_msg)
-                        # self.get_logger().info('Publishing: yaw=%f, pitch=%f, roll=%f' % (point_msg.z, point_msg.y, point_msg.x))
             except serial.SerialException as e:
-                self.get_logger().error('Serial error: %s' % str(e))
+                self.get_logger().error(f'Serial error: {str(e)}')
                 self.reset_serial_connection()
             except Exception as e:
-                self.get_logger().error('Failed to parse serial data: "%s"' % str(e))
+                self.get_logger().error(f'Failed to parse serial data: "{str(e)}"')
                 if 'Input/output error' in str(e):
                     self.reset_serial_connection()
-        else:
-            self.get_logger().warn('Serial port not open. Attempting to reconnect...')
-            self.open_serial_port()
 
     def reset_serial_connection(self):
         if self.serial_port is not None:
             self.serial_port.close()
             self.serial_port = None
+            self.get_logger().info('Serial port reset successfully.')
         self.open_serial_port()
 
 
+    def destroy_node(self):
+        super().destroy_node()
+        if self.serial_port.is_open:
+            self.serial_port.close()
+
 def main(args=None):
     rclpy.init(args=args)
-    serial_subscriber = SerialSubscriber()
-    rclpy.spin(serial_subscriber)
-    serial_subscriber.destroy_node()
-    rclpy.shutdown()
+    node = SerialSubscriber()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
